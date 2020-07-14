febd4664ea40f2695ac7e6436bac270f4500189a
henry lay <henry88lay@gmail.com>
2020-07-14 21:17:48 +1000

    added the set state feature on the popular

--- src/components/popular.js
+++ src/components/popular.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage (selectedLangauge) {
+    this.setState({
+      selectedLangauge
+    })
+  }
   render() {
     const languages = ['All', 'Javascript', 'Ruby', 'Golang', 'CSS', 'Python', 'C#']
 
     return (
       <ul className='flex-center'>
           {languages.map((language) => (
-            <li key={languages}>
-              <button className='btn-clear nav-link'>
+            <li key={language}>
+              <button 
+                className='btn-clear nav-link'
+                style={language === this.state.selectedLanguage ? { color: 'rgb(231,216,187)'} : null }
+                onClick={() => this.updateLanguage(language)}>
                 {language}
               </button>
             </li>
